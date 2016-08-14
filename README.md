6244727b122e7687341b14136d376f4f5163a8d0
Izzy <izzy@qumran.org>
2016-08-14 21:54:55 +0200

    adding class and basic documentation

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # virustotal
-PHP library for VirusTotal.COM
+This is a PHP library for the [VirusTotal.COM
+API](https://www.virustotal.com/de/documentation/public-api/). It's based upon
+[the work of Adrian at
+www.TheWebHelp.com](https://www.thewebhelp.com/php/scripts/virustotal-php-api/).
+As Adrian didn't include a license with his work, I didn't either (I cannot
+put part of his work under a license of my choice, after all).
+
+
+## Requirements
+* PHP (of course; tested with 5.4) with CURL support.
+* an API key (available for free at the VirusTotal website)
+
+## Usage
+Usage is pretty easy. Best demonstrated using an example:
+
+    require_once('virustotal.class.php');
+    $vt = new virustotal($apikey);
+    $res = $vt->checkFile($filename,$hash); // $hash is optional. Pass the $scan_id if you have it, or the file hash
+    switch($res) {
+      case -99: // API limit exceeded
+        // deal with it here – best by waiting a little before trying again :)
+        break;
+      case  -1: // an error occured
+        // deal with it here
+        break;
+      case   0: // no results (yet) – but the file is already enqueued at VirusTotal
+        $scan_id = $vt->getScanId();
+        $json_response = $vt->getResponse();
+        break;
+      case   1: // results are available
+        $json_response = $vt->getResponse();
+        break;
+      default : // you should not reach this point if you've placed the break before :)
+    }
+    // deal with the JSON response here
+
+For details on the JSON response (both on enqueuing a file and when getting
+the final scan results), please consult the [VirusTotal API
+documentation](https://www.virustotal.com/de/documentation/public-api/). For
+details on the PHP class in this repository, consult the code. You should find
+it well documented :)
---
+++ virustotal.class.php
@@ -0,0 +1,183 @@
+<?php
+/** Deal with VirusTotal.com file checks.
+ *  This class will allow you to scan files for viruses using the API from VirusTotal.com.
+ *  You will need an API key (can be obtained for free at https://www.virustotal.com/) to
+ *  use this class. Apart from initializing the class, you will only need to call its
+ *  checkFile() method and, if that finally returns TRUE, obtain your results via the
+ *  getResponse() method.
+ *  Idea taken from a script by Adrian at www.TheWebHelp.com and reworked into a proper
+ *  PHP class by Izzy.
+ * @class virustotal
+ * @author Adrian at www.TheWebHelp.com
+ * @author Izzy at android.izzysoft.de
+ * @see https://www.virustotal.com/de/documentation/public-api/
+ * @see https://www.thewebhelp.com/php/scripts/virustotal-php-api/
+ */
+class virustotal {
+
+  /** API_KEY for the service
+   * @class virustotal
+   * @attribute string api_key
+   */
+  private $api_key = '';
+
+  /** last JSON response from service (or empty if not yet retrieved)
+   * @class virustotal
+   * @attribute protected array json_response
+   * @verbatim important elements (dump it for more details; full elements only when scan completed):
+   *    * positives: number of malware hits (0=clean)
+   *    * total: number of engines used
+   *    * permalink: link to result page
+   *    * scans: detailed result array[name: array[bool detected, str version, str result (name of threat), str update (YYYYMMDD)]]
+   *    * scan_date: YYYY-MM-DD HH24:MI:SS
+   *    * response_code (int), verbose_msg (str)
+   *    * also hashes/identifiers: sha256, sha1, md5, scan_id, resource
+   */
+  protected $json_response = [];
+
+  /** ScanID we can use to query the state for this file
+   * @class virustotal
+   * @attribute protected string scanID
+   */
+  protected $scanID = '';
+
+  /** Initialize the class by setting up the API_KEY
+   * @construct virustotal
+   * @param string api_key VirusTotal API key
+   */
+  function __construct($api_key) {
+    $this->api_key = $api_key;
+  }
+
+  /** Check a file and get the results
+   * @class virustotal
+   * @method checkFile
+   * @info at least one of fileName or file_hash (or both) must be provided -- they cannot be both empty, or we don't know what to check :)
+   * @param optional string fileName    Name of the file to check. We must be able to access it by this name, so include path if needed
+   * @param optional string hash        File Hash (MD5/SHA256) or Scan ID to use. If not passed, hash will be calculated. Scan ID gives more details on queue status.
+   * @return number haveResults         -99: got no response; -1: error; 0: no results, 1: results ready; use self::getResponse() to obtain details;
+   *                                    self::getScanId for the ScanID (set only after initial enqueue, i.e. upload of the file)
+   *                                    other negative values: other errors (most likely unknown / not described in API and should not happen)
+   * @info Note that the -99 (got no response) return code usually means you've exceeded the limits of your key (i.e. 4 requests per minute for a public key)
+   */
+  public function checkFile($fileName='', $hash='') {
+    if ( ! file_exists($fileName) ) {
+      if ( empty($hash) ) {
+        $this->json_response = json_encode(['error'=>"virustotal::checkFile could not find the file specified: '$fileName', and no hash/scanID was provided"]);
+        return -1;
+      } else {
+        $fileNamePassed = $fileName;
+        $fileName = '';
+      }
+    }
+
+    // calculate a hash of this file if not provided, we will use it as an unique ID when quering about this file
+    if ( empty($hash) )
+        $hash = hash_file('sha256', $fileName);
+
+    // first check if a report for this file already exists, so we don't need to upload
+    $report_url = 'https://www.virustotal.com/vtapi/v2/file/report?apikey='.$this->api_key."&resource=".$hash;
+    $api_reply = file_get_contents($report_url);
+    ( $api_reply === '' ) ? $api_reply_array = ['response_code'=>-99,'verbose_msg'=>'Got empty response from VirusTotal'] : $api_reply_array = json_decode($api_reply, true);
+
+    // continue depending on the result
+    switch ( $api_reply_array['response_code'] ) {
+       case -99:  // we've got no response (API limit exceeded?)
+                  $this->json_response = json_encode($api_reply_array);
+                  return -99;
+                  break;
+       case -2 :  // resource is already queued for analysis
+                  $this->json_response = $api_reply;
+                  return 0;
+                  break;
+       case  1 :  // reply is OK (it contains an antivirus report)
+                  $this->json_response = $api_reply;
+                  return 1;
+                  break;
+       case  0 :  // file not yet known to the service
+                  if ( !empty($fileName) ) { // self::json_response will be set by self::uploadFile
+                    if ( $this->uploadFile($fileName) ) return 0; // results are not available immediately
+                    return -1; // an error occured during upload
+                  } else {
+                    $api_reply_array['error'] = "virustotal::checkFile: hash unknown to VirusTotal and file '$fileNamePassed' could not be found";
+                    $this->json_response = json_encode($api_reply_array);
+                    return -1;
+                  }
+                  break;
+       default :  // some error occured
+                  $api_reply_array['error'] = 'API error: '.$api_reply_array['verbose_msg'];
+                  $this->json_response = json_encode($api_reply_array);
+                  return -1;
+                  break;
+    }
+  }
+
+  /** Upload a file to check
+   *  self::checkFile() calls this automatically when needed – so only call this if you're knowing what you're doing :)
+   * @class virustotal
+   * @method uploadFile
+   * @param string fileName Name of the file to check. We must be able to access it by this name, so include path if needed
+   * @return bool success   use self::getResponse() for details, self::getScanId for the ScanID
+   */
+  public function uploadFile($fileName) {
+    if ( ! file_exists($fileName) ) {
+        $this->json_response = json_encode(['error'=>"virustotal::uploadFile could not find the file specified: '$fileName'"]);
+        return FALSE;
+    }
+
+    $file_size_mb = filesize($fileName)/1024/1024;     // get the file size in mb, we will use it to know at what url to send for scanning (it's a different URL for over 30MB)
+
+    $post_url = 'https://www.virustotal.com/vtapi/v2/file/scan';
+    $post['apikey'] = $this->api_key;
+    $post['file'] = '@'.$fileName;
+
+    // get a special URL for uploading files larger than 32MB (up to 200MB)
+    if($file_size_mb >= 32) {// get a special URL for uploading files larger than 32MB (up to 200MB)
+        $api_reply = @file_get_contents('https://www.virustotal.com/vtapi/v2/file/scan/upload_url?apikey='.$this->api_key);
+        $api_reply_array = json_decode($api_reply, true);
+        if ( isset($api_reply_array['upload_url']) and $api_reply_array['upload_url']!='' )
+            $post_url = $api_reply_array['upload_url'];
+    }
+
+    // send the file for checking
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL,$post_url);
+    curl_setopt($ch, CURLOPT_POST,1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
+    $api_reply = curl_exec ($ch);
+    curl_close ($ch);
+
+    // now evaluate results
+    $api_reply_array = json_decode($api_reply, true);
+    if ($api_reply_array['response_code']==1) { // file successfully enqueued
+        $this->scanID = $api_reply_array['scan_id'];
+        $this->json_response = $api_reply;
+        return TRUE;
+    } else {
+        $api_reply_array['error'] = 'API error: '.$api_reply_array['verbose_msg'];
+        $this->json_response = json_encode($api_reply_array);
+        return FALSE;
+    }
+  }
+
+  /** Obtain the ScanID of the latest uploaded file
+   * @class virustotal
+   * @method getScanId
+   * @return string scanId (empty if no file was uploaded by this class instance yet)
+   */
+  public function getScanId() {
+    return $this->scanID;
+  }
+
+  /** Obtain the JSON response array from the latest check
+   * @class virustotal
+   * @method getResponse
+   * @return array response
+   */
+  public function getResponse() {
+    return $this->json_response;
+  }
+
+} // end class virustotal
+?>
